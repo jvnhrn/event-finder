356e7e2573153bb34281abb81f33971f0722f856
Johanna Horn <j.von.horn@hotmail.com>
2021-02-04 18:32:07 +0100

    changing to claudios last commit the explore page

--- src/components/ExplorePage/ExplorePage.jsx
+++ src/components/ExplorePage/ExplorePage.jsx
@@ -1,6 +1,4 @@
-
-import Yoga from '../Categories/Yoga/yoga';
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import ExploreCard from './ExploreCard';
 const axios = require('axios');
 
@@ -22,35 +20,6 @@ function ExplorePage() {
     }
 
     return (
-        
-
-        <div>
-
-        <div>
-                <form className="content-center px-24 pt-10" onSubmit="event.preventDefault();" role="search">
-                    <input className="bg-gray-100 rounded-md px-4 py-4 container focus:ring-purple-600 outline-none" id="search" type="search" placeholder="find out what's popping" />
-                    <button className="hidden absolute inset-0 rounded-md" type="submit">Go</button>    
-                </form>
-        </div>
-
-        <div class="container hidden md:block md:ml-24 md:pr-12 md:space-x-8 py-10 font-bold text-gray-600 text-center tracking-tighter">
-            <a href="../Categories/Yoga/yoga.jsx" class="text-2xl font-light text-grey-500 hover:text-gray-900">Yoga</a>
-            <a href="#" class="text-2xl font-light text-grey-500 hover:text-gray-900">Literature</a>
-            <a href="#" class="text-2xl font-light text-grey-500 hover:text-gray-900">Architecture/Culture</a>
-            <a href="#" class="text-2xl font-light text-grey-500 hover:text-gray-900">Music</a>
-            <a href="#" class="text-2xl font-light text-grey-500 hover:text-gray-900">Artsy-Crafty</a>
-            <a href="#" class="text-2xl font-light text-grey-500 hover:text-gray-900">Random</a>
-         </div>
-
-        <div className = "overflox-x-hidden">
-        <Yoga/>
-        </div>
-            
-
-      
-        
-    </div>
-
         <div class="grid grid-cols-3 gap-2">
             {cardsData.map((card) => {
                 console.log(cardsData);
