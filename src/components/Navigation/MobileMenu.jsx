4b310eb4ea8d49118ee6b40acf3001632bcbd168
Johanna Horn <j.von.horn@hotmail.com>
2021-02-05 12:03:03 +0100

    making the icons work in mobile version

--- src/components/Navigation/MobileMenu.jsx
+++ src/components/Navigation/MobileMenu.jsx
@@ -2,38 +2,25 @@ import React, { useState, useRef, useEffect } from 'react';
 import MobileMenuDropDown from './MobileMenuDropdown';
 
 function MobileMenu () {
-    const [open, setOpen] = useState(false); 
-    const dropit = useRef(null);
+    const [hamburgerOpen, setHamburgerOpen] = useState(false); 
     
-    useEffect(() => {
-        const hideMenu = () =>{
-            if (window.innerWidth > 768 && open){
-                setOpen(false);
-            }
-        };
-        window.addEventListener('resize', hideMenu);
-        return () => {
-            window.removeEventListener('resize', hideMenu)
-        }
-    })
-
     return (
-        <div className="dropdown" ref={dropit}>
+        <div className="dropdown">
             <button class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                onClick={() => setOpen(open => !open)}>
+                >
                 <span class="sr-only">Open main menu</span>
 
-                <svg className={(open ? 'block' : 'hidden')} class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg onClick={() => setHamburgerOpen(hamburgerOpen => !hamburgerOpen)} className={(!hamburgerOpen ? "h-6 w-6" : "hidden h-6 w-6")} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 8h16M4 16h16" />
                 </svg>
 
-                <svg /* className={(setOpen ? 'hidden' : 'hidden')} */ class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg onClick={() => setHamburgerOpen(hamburgerOpen => !hamburgerOpen)} className={(hamburgerOpen ? "h-6 w-6" : "hidden h-6 w-6")} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
                 </svg>
 
             </button>
 
-            {open && <MobileMenuDropDown /> }
+            {hamburgerOpen && <MobileMenuDropDown /> }
         </div> 
     )
 }
