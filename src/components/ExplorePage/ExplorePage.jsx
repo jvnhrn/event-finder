92c71c1e003eb67bec2f5d4237ca864f9d3b9d90
Claudio Capitao <claudio.capitao@gmail.com>
2021-02-05 10:25:59 +0100

    ExplorePage

--- src/components/ExplorePage/ExplorePage.jsx
+++ src/components/ExplorePage/ExplorePage.jsx
@@ -1,45 +1,63 @@
 import React, { useState, useEffect } from 'react';
 import ExploreCard from './ExploreCard';
+import { Button } from 'react-bootstrap';
 const axios = require('axios');
 
 function ExplorePage() {
 
     const [cardsData, setCardsData] = useState([]);
+    const [categoryName, setCategoryName] = useState('all');
 
     useEffect(() => {
-        getAllCardsData();
-    }, [])
-
-    const getAllCardsData = async () => {
-        try {
-            const allCardsData = await axios.get('http://localhost:8080/explorer');
-            await setCardsData(allCardsData.data);
-        } catch (error) {
-            console.error(error);
+        const getCategory = async () => {
+            try {
+                let allCardsData = false;
+                if (categoryName == 'all') {
+                    allCardsData = await axios.get('http://localhost:8080/explorer');
+                } else {
+                    allCardsData = await axios.get(`http://localhost:8080/category/?category=${categoryName}`);
+                }
+                setCardsData(allCardsData.data);
+            } catch (error) {
+                console.error(error);
+            }
         }
-    }
+        getCategory();
+    }, [categoryName]);
 
     return (
-        <div class="grid grid-cols-3 gap-2">
-            {cardsData.map((card) => {
-                console.log(cardsData);
-                return < ExploreCard
-                    event_category={card.event_category}
-                    event_title={card.event_title}
-                    event_description={card.event_description}
-                    event_location={card.event_location}
-                    event_country={card.event_country}
-                    event_city={card.event_city}
-                    event_postalcode={card.event_postalcode}
-                    event_address={card.event_address}
-                    event_host_phone={card.event_host_phone}
-                    event_host_email={card.event_host_email}
-                    event_price={card.event_price}
-                    event_date={card.event_date}
-                    open_spots={card.open_spots}
-                />
-            })}
-        </div>
+        <div>
+            <div className="grid grid-cols-7 gap-2">
+                <Button variant="primary" onClick={() => setCategoryName('all')}>All</Button>
+                <Button variant="primary" onClick={() => setCategoryName('Yoga')}>Yoga</Button>
+                <Button variant="primary" onClick={() => setCategoryName('Festival')}>Festival</Button>
+                <Button variant="primary" onClick={() => setCategoryName('Restaurant')}>Restaurant</Button>
+                <Button variant="primary" onClick={() => setCategoryName('Literature')}>Literature</Button>
+            </div>
+
+            < div className="grid grid-cols-3 gap-2" >
+                {
+                    cardsData.map((card) => {
+                        console.log(cardsData);
+                        return < ExploreCard
+                            event_category={card.event_category}
+                            event_title={card.event_title}
+                            event_description={card.event_description}
+                            event_location={card.event_location}
+                            event_country={card.event_country}
+                            event_city={card.event_city}
+                            event_postalcode={card.event_postalcode}
+                            event_address={card.event_address}
+                            event_host_phone={card.event_host_phone}
+                            event_host_email={card.event_host_email}
+                            event_price={card.event_price}
+                            event_date={card.event_date}
+                            open_spots={card.open_spots}
+                        />
+                    })
+                }
+            </div >
+        </div >
     )
 }
 
