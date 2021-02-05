93a9e75252adc7a394b8170a03854ef2dcf0002a
Johanna Horn <j.von.horn@hotmail.com>
2021-02-05 14:22:08 +0100

    Trying To get some more styling done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Logo from '../Navigation/Logo';
 
 function Footer() {
     return (
-        <div class="bg-gray-700 w-full mx-auto px-4 py-6 lg:h-20 md:h-20 sm:h-auto absolute bottom-0 left-0">
+        <div class="bg-gray-800 w-full mx-auto px-4 py-6 lg:h-20 md:h-20 sm:h-auto absolute bottom-0 left-0">
             <div class="flex items-center justify-between max-w-7xl mx-auto">
-                <div class="flex items-center">
-                    <div class="flex items-center">
-                        Logo
-                    </div>
-
+                <div class="flex space-x-8 items-center">
+                    <Logo />
                     {/* large Menu - NavLinks */}
-                    <div class="md:block ">
+                    <div class="grid justify-items-end">
                         <div class="ml-10 flex items-baseline space-x-4">
 
-                            <NavLink to='/' class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</NavLink>
+                            <Link to='/' class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</Link>
 
-                            <NavLink to='/search' class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Search</NavLink>
+                            <Link to='/search' class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Search</Link>
 
-                            <NavLink to='/explore' class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Explore</NavLink>
+                            <Link to='/explore' class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Explore</Link>
 
-                            <NavLink to='/hostevent' class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Host an Event</NavLink>
+                            <Link to='/hostevent' class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Host an Event</Link>
                         </div>
                     </div> 
                 </div>
--- src/components/Navigation/TopNav.jsx
+++ src/components/Navigation/TopNav.jsx
@@ -4,6 +4,7 @@ import ProfilDropdown from './ProfilDropdown';
 import Notification from './Notification';
 import MobileMenu from './MobileMenu';
 import Logo from './Logo';
+import ProfilIcon from './ProfilIcon';
 
 
 function Navigation() {
@@ -56,8 +57,7 @@ function Navigation() {
                                         
                                             <button class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu" aria-haspopup="true"
                                              onClick={()=> setShow(show => !show)} >
-                                                <span class="sr-only">Open user menu</span>
-                                                <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
+                                                <ProfilIcon />
                                             </button>
                                         
                                         {show && <ProfilDropdown /> }
