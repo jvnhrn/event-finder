06effe57db2f796401cfe177e83659962bb2f7da
Johanna Horn <j.von.horn@hotmail.com>
2021-02-08 14:49:17 +0100

    seperating some elements into single elements for mobile version

--- src/components/Navigation/MobileMenuDropdown.jsx
+++ src/components/Navigation/MobileMenuDropdown.jsx
@@ -1,31 +1,33 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import MobileProfilIcon from './MobileProfil';
+import MobileProfilLinks from './MobileProfilLinks';
+import MobileUserInfo from './MobileUserInfo';
 
 const MobileMenuDropDown = () =>{
 
+    const [showMobile, setShowMobile] = useState (false);
+
+    const hide = () => {
+        setShowMobile(!showMobile)
+    };
+    
     return (
         <div class="block bg-gray-800">
-            <div class="px-2 pt-14 pb-3">
+            <div class="pt-14 pb-3">
 
-                    <NavLink to='/' class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">Home</NavLink>
+                <Link to='/' onClick={hide} class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium">Home</Link>
 
-                    <NavLink to="/search" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Search</NavLink>
+                <Link to="/explore" onClick={hide} class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Explore</Link>
 
-                    <NavLink to="/explore" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Explore</NavLink>
-
-                    <NavLink to="/hostevent" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Host an Event</NavLink>
+                <Link to="/hostevent" onClick={hide} class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Host an Event</Link>
 
             </div>
 
             <div class="pt-4 pb-3 border-t border-gray-700">
-                <div class="flex items-center px-5">
-                    <div class="flex-shrink-0">
-                        <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                    </div>
-                    <div class="ml-3">
-                        <div class="text-base font-medium leading-none text-white">Tom Cook</div>
-                        <div class="text-sm font-medium leading-none text-gray-400">tom@example.com</div>
-                    </div>
+                <div class="flex items-center">
+                    <MobileProfilIcon />
+                    <MobileUserInfo />
                     <button class="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                         <span class="sr-only">View notifications</span>
                         <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
@@ -33,13 +35,7 @@ const MobileMenuDropDown = () =>{
                         </svg>
                     </button>
                 </div>
-                <div class="mt-3 px-2 space-y-1">
-                    <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">Your Profile</a>
-
-                    <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">Settings</a>
-
-                    <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">Sign out</a>
-                </div>
+                <MobileProfilLinks />
             </div>
         </div>
     )
---
+++ src/components/Navigation/MobileProfilLinks.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MobileProfilLinks = () => {
+
+    return (
+        <div class="mt-3 px-2 space-y-1">
+            <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">Your Profile</a>
+
+            <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">Settings</a>
+
+            <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">Sign out</a>
+        </div>
+    )
+}
+
+export default MobileProfilLinks; 
