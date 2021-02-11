2a42209129e00f4383df5ce243e232c07f003217
jessy <jessynnebs@gmail.com>
2021-02-11 10:12:06 +0100

    styling all pages

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -39,7 +39,7 @@ function LandingPage() {
       }
    
     return (
-        <div class="container">
+        <div class="container pr-2">
 
             <div>
                 <form className="content-center pl-8 ml-12 pt-6" onSubmit="event.preventDefault();" role="search">
