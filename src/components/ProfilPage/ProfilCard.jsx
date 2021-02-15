106ac55e2e8bc0ce9a29083258cb7dd71a63c794
Johanna Horn <j.von.horn@hotmail.com>
2021-02-15 18:47:13 +0100

    having some issues with the profilpage - styling

--- src/components/ProfilPage/ProfilCard.jsx
+++ src/components/ProfilPage/ProfilCard.jsx
@@ -4,9 +4,12 @@ import hotpodyoga from '../Categories/Yoga/hotpodyoga.png';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actions from '../actions/app.actions'
+import ProfilDetails from './ProfilDetails';
 
 
 function ProfilCard(props) {
+
+    const [show, setShow] = useState(false);
 
     let history = useHistory();
 
@@ -90,7 +93,8 @@ function ProfilCard(props) {
                         </li>
                         <li class="flex mt-8">
                                 <div class="m-auto">
-                                    <button class="font-bold text-base flex items-center justify-center border-blue-600 border-2 py-2 px-4 rounded outline-none shadow-lg focus:outline-none text-blue-600 hover:bg-blue-100 hover:border-blue-100 hover:shadow-xl">
+                                    <button class="font-bold text-base flex items-center justify-center border-blue-600 border-2 py-2 px-4 rounded outline-none shadow-lg focus:outline-none text-blue-600 hover:bg-blue-100 hover:border-blue-100 hover:shadow-xl"
+                                    onClick={() => setShow(show => !show)}>
                                     <svg class="h-8 w-8 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                                     </svg> Edit your details
@@ -112,6 +116,8 @@ function ProfilCard(props) {
                 </div>
                </div> 
             </div>
+            {show ? <ProfilDetails /> : ""}
+            
         </div>
     )
 }
--- src/components/ProfilPage/ProfilDetails.jsx
+++ src/components/ProfilPage/ProfilDetails.jsx
@@ -66,7 +66,7 @@ const ProfilDetails = () =>{
     }, [profil_image])
 
     return(
-        <div class="container mx-auto">
+        <div class="container fixed justify-center top-16 py-12 bg-white">
             <div {...getRootProps({ className: 'dropzone' }) } class=" flex justify-center container px-6 py-6 border-2 border-gray-300 border-dashed rounded-full" >
                 <div class="space-y-1 text-center">
                     <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
@@ -88,125 +88,128 @@ const ProfilDetails = () =>{
             </div >
 
        
-                            
-                    <ul id="details" className="py-4 text-left tracking-tighter">
-                        <li>
+            <div class="mx-4">          
+                <ul id="details" className="py-4 text-left tracking-tighter">
+                    <li class="md:flex mt-5 justify-center">
                             <strong className="text-gray-400 ">Details</strong>
                         </li>
-                        <li class="flex mt-5">
-                        <div id="userid" class="h-6 w-6 mr-2 py-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                                </svg>
+                        <li class="md:flex mt-5 justify-center">
+                        <div class="flex">
+                            <div id="userid" class="h-6 w-6 mr-2 py-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                                    </svg>
                             </div>
                             <div>
                                 <label class="block text-gray-700 font-bold py-2 ml-3 w-36" for="text">User name</label>
                             </div>
-                            <div>
-                                <input onChange={(e) => set_user_name(e.target.value)} type="text" name="postal_code" id="event_name" class="tracking-tighter bg-gray-100 text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 py-2" placeholder="Username" />
-                            </div>
+                        </div>
+                        <div>
+                            <input onChange={(e) => set_user_name(e.target.value)} type="text" name="postal_code" id="event_name" class="tracking-tighter md:w-96 w-72 bg-gray-100 text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 py-2" placeholder="Username" />
+                        </div>
                         </li>
-                        <li class="flex mt-5">
-                            <div id="phone" class="h-6 w-6 mr-2 py-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
-                                </svg>
+                        <li class="md:flex mt-5 justify-center">
+                            <div class="flex">
+                                <div id="phone" class="h-6 w-6 mr-2 py-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+                                    </svg>
+                                </div>
+                                <div>
+                                    <label class="block text-gray-700 font-bold py-2 ml-3 w-36" for="text">Phone number</label>    
+                                </div>
                             </div>
                             <div>
-                                <label class="block text-gray-700 font-bold py-2 ml-3 w-36" for="text">Phone number</label>    
-                            </div>
-                            <div>
-                                <input onChange={(e) => set_user_phone(e.target.value)} type="text" id="password2" class="tracking-tighter bg-gray-100 text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 py-2" />
+                            <input onChange={(e) => set_user_phone(e.target.value)} type="text" id="password2" class="tracking-tighter md:w-96 w-72 bg-gray-100 text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 py-2" />
                             </div>
 
                         </li>
-                        <li class="flex mt-5">
-                            <div id="email" class="h-6 w-6 mr-2 py-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
-                                </svg>
+                        <li class="md:flex mt-5 justify-center">
+                            <div class="flex">
+                                <div id="email" class="h-6 w-6 mr-2 py-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+                                    </svg>
+                                </div>
+                                <div>
+                                    <label class="block text-gray-700 font-bold py-2 ml-3 w-36" for="email">Email</label>
+                                </div>
                             </div>
-                            <div>
-                                <label class="block text-gray-700 font-bold py-2 ml-3 w-36" for="email">Email</label>
-                                
-                            </div>
-                            <div>
-                                <input onChange={(e) => set_user_email(e.target.value)} type="email" id="password2" class="tracking-tighter bg-gray-100 text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 py-2" />
-                            </div>
-                        </li>
-                        <li class="flex mt-5">
-                            <div id="email" class="h-6 w-6 mr-2 py-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2" />
-                                </svg>
-                            </div>
-                            <div>
-                                <label class="block text-gray-700 font-bold py-2 ml-3 w-36" for="email">Firstname</label>
-
-                            </div>
-                            <div>
-                                <input onChange={(e) => set_user_first_name(e.target.value)} type="email" id="password2" class="tracking-tighter bg-gray-100 text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 py-2" />
+                            <div class="">
+                            <input onChange={(e) => set_user_email(e.target.value)} type="email" id="password2" class="tracking-tighter md:w-96 w-72 bg-gray-100 text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 py-2" />
                             </div>
                         </li>
-                        <li class="flex mt-5">
-                            <div id="email" class="h-6 w-6 mr-2 py-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2" />
-                                </svg>
+                        <li class="md:flex mt-5 justify-center">
+                            <div class="flex">
+                                <div id="email" class="h-6 w-6 mr-2 py-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2" />
+                                    </svg>
+                                </div>
+                                <div>
+                                    <label class="block text-gray-700 font-bold py-2 ml-3 w-36" for="email">Firstname</label>
+                                </div>
                             </div>
                             <div>
-                                <label class="block text-gray-700 font-bold py-2 ml-3 w-36" for="email">Lastname</label>
-
-                            </div>
-                            <div>
-                            <input onChange={(e) => set_user_last_name(e.target.value)} type="email" id="password2" class="tracking-tighter bg-gray-100 text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 py-2" />
+                            <input onChange={(e) => set_user_first_name(e.target.value)} type="email" id="password2" class="tracking-tighter md:w-96 w-72 bg-gray-100 text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 py-2" />
                             </div>
                         </li>
-                        <li class="flex mt-5">
-                            <div id="email" class="h-6 w-6 mr-2 py-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2" />
-                                </svg>
+                        <li class="md:flex mt-5 justify-center">
+                            <div class="flex">
+                                <div id="email" class="h-6 w-6 mr-2 py-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2" />
+                                    </svg>
+                                </div>
+                                <div>
+                                    <label class="block text-gray-700 font-bold py-2 ml-3 w-36" for="email">Lastname</label>
+                                </div>
                             </div>
                             <div>
-                                <label class="block text-gray-700 font-bold py-2 ml-3 w-36" for="email">Password</label>
-
-                            </div>
-                            <div>
-                                <input onChange={(e) => set_user_password(e.target.value)} type="password" id="password1" class="tracking-tighter bg-gray-100 text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 py-2" />
+                            <input onChange={(e) => set_user_last_name(e.target.value)} type="email" id="password2" class="tracking-tighter md:w-96 w-72 bg-gray-100 text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 py-2" />
                             </div>
                         </li>
-                        <li class="flex mt-5">
-                            <div id="email" class="h-6 w-6 mr-2 py-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2" />
-                                </svg>
+                        <li class="md:flex mt-5 justify-center">
+                            <div class="flex">
+                                <div id="email" class="h-6 w-6 mr-2 py-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z" />
+                                    </svg>
+                                </div>
+                                <div>
+                                    <label class="block text-gray-700 font-bold py-2 ml-3 w-36" for="email">Password</label>
+                                </div>
                             </div>
                             <div>
-                                <label class="block text-gray-700 font-bold py-2 ml-3 w-36" for="email">Repeat Password</label>
-
+                            <input onChange={(e) => set_user_password(e.target.value)} type="password" id="password1" class="tracking-tighter md:w-96 w-72 bg-gray-100 text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 py-2" />
+                            </div>
+                        </li>
+                        <li class="md:flex mt-5 justify-center">
+                            <div class="flex">
+                                <div id="email" class="h-6 w-6 mr-2 py-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z" />
+                                    </svg>
+                                </div>
+                                <div>
+                                    <label class="block text-gray-700 font-bold py-2 ml-3 w-36" for="email">Repeat Password</label>
+                                </div>
                             </div>
                             <div>
-                            <input onChange={(e) => set_user_password_repeat(e.target.value)} type="password" id="password1" class="tracking-tighter bg-gray-100 text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 py-2" />
+                            <input onChange={(e) => set_user_password_repeat(e.target.value)} type="password" id="password1" class="tracking-tighter md:w-96 w-72 bg-gray-100 text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 py-2" />
                             </div>
                         </li>
                     </ul>
 
-                
-                    <div class="mb-6 pt-3 rounded">
-                        <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="date">Date of Birth</label>
-                        <input onChange={(e) => set_user_birthdate(e.target.value)} type="date" id="password3" class="tracking-tighter bg-gray-100 text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-blue-600 transition duration-500 px-3 py-2" />
-                    </div>
-                  
-                    
+                </div> 
+
                     <div class="mb-6 rounded bg-red-200">
                         {wrongPassword}
                     </div>
                     {!!userRegistered ? <div class="mb-6 rounded bg-red-200"> User (username, email or phone number) already exists</div> : <div></div>}
                     <div class="flex justify-center">
-                            <button onClick={changeUserDetails} class="bg-blue-600 hover:bg-blue-800 text-white font-bold uppercase py-2 px-4 rounded shadow-lg hover:shadow-xl outline-none focus:outline-none transition duration-200" /* type="submit" */>Edit</button>
+                <button onClick={changeUserDetails} class="bg-blue-600 hover:bg-blue-800 text-white font-bold uppercase py-2 px-4 rounded shadow-lg hover:shadow-xl outline-none focus:outline-none transition duration-200" /* type="submit" */>Edit</button>
                     </div> {/* </form> */}
-
 
         </div>
     )
