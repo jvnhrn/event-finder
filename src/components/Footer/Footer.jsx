7aa9f42368aead70d8b97d4df91b8ad5dc82065a
Johanna Horn <j.von.horn@hotmail.com>
2021-02-16 17:36:24 +0100

    chaning something in thwe footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -36,28 +36,33 @@ function Footer(props) {
             <div class="flex items-center justify-between max-w-7xl mx-auto space-x-8">
             <div class="grid md:grid-cols-3 md:gap-4 mx-auto text-gray-400 my-4 lg:w-full lg:px-10 md:w-full sm:grid-cols-none">
                 <div class="..."> <LogoFooter/> </div>
-                <div class="... text-center hidden md:block">
-                <Link to='/contact' class="text-gray-400 hover:text-gray-200 py-2 rounded-md text-md font-regular">Contact</Link> 
+                <div class="... text-left hidden md:block">
+                          
                 </div>
-                <div class="... text-right hidden md:block text-md">Navigate</div>
+                <div class="... text-right hidden md:block text-md"></div>
                 <div class="..."> 
                         <div class="text-left hidden md:block items-center"> 
                         eventi:
                         This project is developed within the Codemaster Bootcamp from Talent Garden Austria.
                     </div>
                 </div>
-                <div class="... text-center hidden md:block">Rainbow Road 7</div>
-                <div class="... hidden md:block">
+                <div class="... text-center hidden md:block">
+                    
+                </div>
+                    <div class="... hidden md:block">
 
-                    <div class="grid grid-cols-1 gap-4 text-right flex items-baseline">
+                    {/* <div class="grid grid-cols-1 gap-4 text-right flex items-baseline">
 
                         <Link to='/' class="text-gray-400 hover:text-gray-200 py-2 rounded-md text-md font-regular">Home</Link>
 
                         <Link to='/explore' class="text-gray-400 hover:text-gray-200 py-2 rounded-md text-md font-regular">Explore</Link>
 
                         <Link to='/hostevent' class="text-gray-400 hover:text-gray-200 py-2 rounded-md text-md font-regular">Host an Event</Link>
-                    </div>
-                </div>
+                    </div>  */}
+
+                        <p class="text-left mb-3">Do you have a question about eventi?? Or you just wanna say hi</p>
+                        <Link to='/contact' class=" py-1 text-gray-600 font-bold rounded-md text-md hover:underline hover:bg-red-100 hover:text-red-800">Get in touch with us!</Link>
+                </div> 
             </div>
             </div>
         </div>
