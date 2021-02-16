6fc34484fb17ad4f9ce865d0432f52a6cb4ea737
Claudio Capitao <claudio.capitao@gmail.com>
2021-02-16 10:52:12 +0100

    package.jason, LandinggPage.js, EventMarker.js, ExplorePage.js, ExploreCard.js, main.css, index.html

--- src/components/ExplorePage/ExplorePage.jsx
+++ src/components/ExplorePage/ExplorePage.jsx
@@ -1,12 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import ExploreCard from './ExploreCard';
 import axios from 'axios';
+import { divIcon } from 'leaflet';
+import opencage from 'opencage-api-client';
 
 function ExplorePage() {
 
     const [cardsData, setCardsData] = useState([]);
     const [category, setCategory] = useState('All');
     const [searchText, setSearchText] = useState('');
+
+    const [searchAddress, setSearchAddress] = useState('');
+    const [newAdddressGPSCoordinates, setNewAdddressGPSCoordinates] = useState({ lat: 0, lng: 0 })
+    const [listAddresses, setListAddresses] = useState([])
+    const [showAddressButton, setShowAddressButton] = useState(false)
+    const [selectedAddress, setSelectedAddress] = useState('')
+
+    const [distanceRange, setDistanceRange] = useState(10)
+
+    const [selectionStartDate, setSelectionStartDate] = useState('');
+    const [selectionEndDate, setSelectionEndDate] = useState('');
+
+    // Address search and map positioning
+    useEffect(() => {
+        const getCoordinates = async () => {
+            try {
+                const gpsCoordinates = await opencage.geocode({ q: searchAddress, key: 'dbcf492b8db545538b8b5d9acbeb0313' });
+                console.log(gpsCoordinates.results);
+                setListAddresses(gpsCoordinates.results)
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        getCoordinates()
+    }, [searchAddress])
+
+    useEffect(() => {
+        if (!!searchAddress.length) {
+            setShowAddressButton(true)
+        } else {
+            setShowAddressButton(false)
+            setNewAdddressGPSCoordinates({ lat: 0, lng: 0 })
+        }
+    }, [searchAddress])
 
     useEffect(() => {
         const getAllCards = async () => {
@@ -46,23 +82,174 @@ function ExplorePage() {
     useEffect(() => {
         const getSearch = async () => {
             try {
-                const searchData = await axios.get(`http://localhost:7777/search/?searchtext=${searchText}`);
+                const searchData = await axios.get(`http://localhost:7777/search/?searchtext=${searchText}&&distanceRange=${distanceRange}&&selectionStartDate=${selectionStartDate}&&selectionEndDate=${selectionEndDate}&&gpsLat=${newAdddressGPSCoordinates.lat}&&gpsLng=${newAdddressGPSCoordinates.lng}`);
                 setCardsData(searchData.data);
+                console.log(searchData.data);
             } catch (error) {
                 console.error(error);
             }
         }
         getSearch()
-    }, [searchText]);
+    }, [searchText, distanceRange, selectionStartDate, selectionEndDate, searchAddress, newAdddressGPSCoordinates]);
+
+
+    const [orderByTitleState, setOrderByTitleState] = useState(false)
+    useEffect(() => {
+        const orderByTitle = () => {
+            const newCardsData = [...cardsData];
+            console.log(newCardsData)
+            const sortArray = (first, second) => {
+                newCardsData.sort(function (a, b) {
+                    var nameA = a.event_title.toUpperCase(); // ignore upper and lowercase
+                    var nameB = b.event_title.toUpperCase(); // ignore upper and lowercase
+                    if (nameA < nameB) {
+                        return first;
+                    }
+                    if (nameA > nameB) {
+                        return second;
+                    }
+                    return 0;
+                });
+            }
+            if (orderByTitleState) {
+                sortArray(-1, 1)
+            } else if (!orderByTitleState) {
+                sortArray(1, -1)
+            }
+            console.log(newCardsData)
+            setCardsData(newCardsData);
+            console.log(cardsData)
+        }
+        orderByTitle()
+    }, [orderByTitleState])
+
+
+
+    const [orderByDateState, setOrderByDateState] = useState(false)
+    useEffect(() => {
+        const orderByDate = () => {
+            const newCardsData = [...cardsData];
+            console.log(newCardsData)
+            const sortArray = (first, second) => {
+                newCardsData.sort(function (a, b) {
+                    var nameA = a.event_start_date.toUpperCase(); // ignore upper and lowercase
+                    var nameB = b.event_start_date.toUpperCase(); // ignore upper and lowercase
+                    if (nameA < nameB) {
+                        return first;
+                    }
+                    if (nameA > nameB) {
+                        return second;
+                    }
+                    return 0;
+                });
+            }
+            if (orderByDateState) {
+                sortArray(-1, 1)
+            } else if (!orderByDateState) {
+                sortArray(1, -1)
+            }
+            console.log(newCardsData)
+            setCardsData(newCardsData)
+            console.log(cardsData)
+        }
+        orderByDate()
+    }, [orderByDateState])
+
+    const [orderByPriceState, setOrderByPriceState] = useState(false)
+    useEffect(() => {
+        const orderByPrice = () => {
+            const newCardsData = [...cardsData];
+            console.log(newCardsData)
+            if (orderByPriceState) {
+                newCardsData.sort(function (a, b) {
+                    return a.event_price - b.event_price
+                });
+            } else if (!orderByPriceState) {
+
+                newCardsData.sort(function (a, b) {
+                    return b.event_price - a.event_price
+                });
+            }
+            console.log(newCardsData)
+            setCardsData(newCardsData);
+            console.log(cardsData)
+        }
+        orderByPrice()
+    }, [orderByPriceState])
+
 
     return (
         <div>
             <div className="container">
-
                 <form className="content-center pl-8 ml-12 pt-6" value={searchText} onChange={(e) => { setSearchText(e.target.value) }} role="search">
-
                     <input className="bg-gray-100 rounded-md px-4 py-2 container focus:ring-purple-600 outline-none tracking-tighter" /* id="search" type="search" */ placeholder="find out what's popping" />
                     <button className="hidden absolute inset-0 rounded-md" type="submit">Go</button>
+                </form>
+            </div>
+
+            <div className="container">
+                <form className="content-left pl-8 ml-12 pt-6" value={selectionStartDate} onChange={(e) => { setSelectionStartDate(e.target.value) }} role="search">
+                    <input className="bg-gray-100 rounded-md px-4 py-2 container focus:ring-purple-600 outline-none tracking-tighter" type="datetime-local" placeholder="Start" />
+                    <button className="hidden absolute inset-0 rounded-md" type="submit">Go</button>
+                </form>
+            </div>
+
+            <div className="container">
+                <form className="content-left pl-8 ml-12 pt-6" value={selectionEndDate} onChange={(e) => { setSelectionEndDate(e.target.value) }} role="search">
+                    <input className="bg-gray-100 rounded-md px-4 py-2 container focus:ring-purple-600 outline-none tracking-tighter" type="datetime-local" placeholder="Final" />
+                    <button className="hidden absolute inset-0 rounded-md" type="submit">Go</button>
+                </form>
+            </div>
+
+            <div>
+                <form className="content-left pl-8 ml-12 pt-6">
+                    <input value={searchAddress}
+                        onChange={(e) => { setSearchAddress(e.target.value) }}
+                        className="bg-gray-100 rounded-md px-4 py-2 container focus:ring-purple-600 outline-none tracking-tighter"
+                        placeholder="address, city, country"
+                        type="text"
+                    />
+                    <button className="hidden absolute inset-0 rounded-md" type="submit">Go</button>
+                </form>
+
+                <div class="origin-top-right absolute left-0 mt-2 w-auto rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                    <div class="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                        {
+                            listAddresses.map((address) => {
+                                return (
+                                    <form>
+                                        <button
+                                            onClick={() => {
+                                                setSearchAddress(address.formatted)
+                                                setNewAdddressGPSCoordinates(address.geometry);
+                                                setShowAddressButton(false)
+                                                setSelectedAddress(address.formatted)
+                                            }}
+                                            type='button'
+                                            className={
+                                                showAddressButton && !!searchAddress.length && selectedAddress != searchAddress
+                                                    ?
+                                                    "block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+                                                    :
+                                                    "hidden block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+                                            }
+                                        >
+                                            {address.formatted}
+                                        </button>
+                                    </form>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="container">
+                <form className="content-left pl-8 ml-12 pt-6" >
+                    <div>Ditance from address</div>
+                    <input value={distanceRange} onChange={(e) => { setDistanceRange(e.target.value) }} type="range" min="2" max="20" step="2" />
+                    <div>{distanceRange} Km</div>
                 </form>
             </div>
 
@@ -72,6 +259,12 @@ function ExplorePage() {
                 <button onClick={() => setCategory('Festival')} class="focus:outline-none text-md font-light text-grey-500 hover:text-gray-400" >Festival</button>{''}
                 <button onClick={() => setCategory('Literature')} class="focus:outline-none text-md font-light text-grey-500 hover:text-gray-400" >Literature</button>{''}
                 <button onClick={() => setCategory('Restaurant')} class="focus:outline-none text-md font-light text-grey-500 hover:text-gray-400" >Restaurant</button>
+            </div>
+
+            <div class="container hidden md:block md:ml-12 md:pr-12 md:space-x-8 py-6 text-center tracking-tighter" >
+                <button onClick={() => { setOrderByTitleState(!orderByTitleState) }} class="focus:outline-none text-md font-light text-grey-500 hover:text-gray-400" >Title</button>{''}
+                <button onClick={() => { setOrderByDateState(!orderByDateState) }} class="focus:outline-none text-md font-light text-grey-500 hover:text-gray-400" >Date</button>{''}
+                <button onClick={() => { setOrderByPriceState(!orderByPriceState) }} class="focus:outline-none text-md font-light text-grey-500 hover:text-gray-400" >Price</button>{''}
             </div>
 
             < div className="grid grid-cols-3 gap-2" >
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,35 +1,95 @@
 import { React, useState, useEffect } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, useMapEvents, LayersControl } from 'react-leaflet';
+import { MapContainer, ControlledLayer, useMap, MapConsumer, TileLayer, Marker, Popup, useMapEvent, useMapEvents, LayersControl } from 'react-leaflet';
 import markerYoureHerePng from "../LandingPage/map_markers/youre-here.png";
-import yoga from "../LandingPage/map_markers/yoga.png";
-import food from "../LandingPage/map_markers/food.png";
-import literature from "../LandingPage/map_markers/literature.png";
-import architecture from "../LandingPage/map_markers/architecture.png";
-import music from "../LandingPage/map_markers/music.png";
-import pottery from "../LandingPage/map_markers/pottery.png";
-import random from "../LandingPage/map_markers/random.png";
+import markerYoureHereBW from "../LandingPage/map_markers/youre-hereBW.png";
 import { Icon } from 'leaflet';
+import opencage from 'opencage-api-client';
 
 import axios from 'axios';
+import ContainerDimensions from 'react-container-dimensions'
 import EventMarker from './EventMarker';
-const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
-
 
 function LandingPage() {
 
-  const [position, setPosition] = useState(null)
-  const [eventsData, setEventsData] = useState([]);
+  const [searchAddress, setSearchAddress] = useState('');
+  const [newAdddressGPSCoordinates, setNewAdddressGPSCoordinates] = useState({ lat: 48.210033, lng: 16.363449 })
+  const [listAddresses, setListAddresses] = useState([])
+  const [showAddressButton, setShowAddressButton] = useState(false)
+  const [selectedAddress, setSelectedAddress] = useState('')
+
   const [searchText, setSearchText] = useState('');
+  const [eventsData, setEventsData] = useState([]);
+
+  const [position, setPosition] = useState(null)
+
+  //Collect all data all events
+  useEffect(() => {
+    const getAllEvents = async () => {
+      try {
+        const allEventsData = await axios.get('http://localhost:7777/explore');
+        setEventsData(allEventsData.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getAllEvents()
+  }, []);
 
 
+  // Address search and map positioning
+  useEffect(() => {
+    const getCoordinates = async () => {
+      try {
+        const gpsCoordinates = await opencage.geocode({ q: searchAddress, key: 'dbcf492b8db545538b8b5d9acbeb0313' });
+        console.log(gpsCoordinates.results);
+        setListAddresses(gpsCoordinates.results)
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getCoordinates()
+  }, [searchAddress])
+
+  function NewAdress() {
+    const map = useMap()
+    map.setView(newAdddressGPSCoordinates, map.getZoom(20));
+    return (
+      <Marker
+        position={newAdddressGPSCoordinates}
+        icon={new Icon({ iconUrl: markerYoureHereBW })}>
+        <Popup>{searchAddress}</Popup>
+      </Marker>
+    )
+  }
+
+  useEffect(() => {
+    if (!!searchAddress.length) {
+      setShowAddressButton(true)
+    } else { setShowAddressButton(false) }
+  }, [searchAddress])
+
+  // Search for an event
+  useEffect(() => {
+    const getEventsSearch = async () => {
+      try {
+        const searchTextData = await axios.get(`http://localhost:7777/getsearchData/?searchtext=${searchText}`);
+        console.log(searchTextData.data)
+        setEventsData(searchTextData.data);
+
+        /* } */
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getEventsSearch()
+  }, [searchText]);
+
+  // Locate current position
   function LocationMarker() {
-
     const map = useMapEvents({
-
       click() {
         map.locate()
       },
-
       locationfound(e) {
         setPosition(e.latlng)
         map.flyTo(e.latlng, map.getZoom())
@@ -46,111 +106,124 @@ function LandingPage() {
     )
   }
 
-  useEffect(() => {
-    const getAllEvents = async () => {
-      try {
-        const allEventsData = await axios.get('http://localhost:7777/explore');
-        setEventsData(allEventsData.data);
-        /* console.log(allEventsData.data) */
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    getAllEvents()
-  }, []);
-
-  useEffect(() => {
-    const getSearch = async () => {
-      try {
-        const searchData = await axios.get(`http://localhost:7777/search/?searchtext=${searchText}`);
-        setEventsData(searchData.data);
-        /* console.log(searchData.data) */
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    getSearch()
-  }, [searchText]);
-
   return (
-    <div class="container">
+    <div>
+      <div className="relative z-50 grid grid-cols-2 gap-4">
+        <div>
+          <form className="content-center px-24 pt-6">
+            <input value={searchAddress}
+              onChange={(e) => { setSearchAddress(e.target.value) }}
+              className="bg-gray-100 rounded-md px-4 py-2 container focus:ring-purple-600 outline-none tracking-tighter"
+              placeholder="address, city, country"
+              type="text"
+            />
+          </form>
 
-      <div>
-        <form className="content-center px-24 pt-6" value={searchText} onChange={(e) => { setSearchText(e.target.value) }} role="search">
-          <input className="bg-gray-100 rounded-md px-4 py-2 container focus:ring-purple-600 outline-none tracking-tighter" /* id="search" type="search" */ placeholder="find out what's popping" />
-          <button className="hidden absolute inset-0 rounded-md" type="submit">Go</button>
-        </form>
+          <div class="origin-top-right absolute left-0 mt-2 w-auto rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+            <div class="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+              {
+                listAddresses.map((address) => {
+                  return (
+                    <form>
+                      <button
+                        onClick={() => {
+                          setSearchAddress(address.formatted)
+                          setNewAdddressGPSCoordinates(address.geometry);
+                          setShowAddressButton(false)
+                          setSelectedAddress(address.formatted)
+                        }}
+                        type='button'
+                        className={
+                          showAddressButton && !!searchAddress.length && selectedAddress != searchAddress
+                            ?
+                            "block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+                            :
+                            "hidden block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+                        }
+                      >
+                        {address.formatted}
+                      </button>
+                    </form>
+                  )
+                })
+              }
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <form className="content-center px-24 pt-6"
+            value={searchText}
+            onChange={(e) => { setSearchText(e.target.value) }}
+          >
+            <input className="bg-gray-100 rounded-md px-4 py-2 container focus:ring-purple-600 outline-none tracking-tighter"
+              placeholder="find out what's popping"
+              type="text"
+            />
+          </form>
+        </div>
       </div>
 
-      <MapContainer
-        className="map"
-        center={{ lat: 48.210033, lng: 16.363449 }}
-        zoom={13}
-        scrollWheelZoom={true}
-      >
 
-        <LayersControl position="topright">
+      <div id="mapid" className="relative z-0">
+        <div className="container" >
+          <MapContainer
+            className="map h-480"
+            center={[48.210033, 16.363449]}
+            zoom={13}
+            scrollWheelZoom={true}
+          >
 
-          <LayersControl.BaseLayer checked name="Light">
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-              url='https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'
-            />
-          </LayersControl.BaseLayer>
+            <LayersControl position="topright" >
 
-          <LayersControl.BaseLayer name="Descriptive">
-            <TileLayer
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-          </LayersControl.BaseLayer>
+              <LayersControl.BaseLayer checked name="Light">
+                <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                  url='https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'
+                />
+              </LayersControl.BaseLayer>
 
+              <LayersControl.BaseLayer name="Descriptive">
+                <TileLayer
+                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+              </LayersControl.BaseLayer>
 
-          <LayersControl.BaseLayer name="Light 02">
-            <TileLayer
-              attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-              url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
-            />
-          </LayersControl.BaseLayer>
+              <NewAdress />
+              <LocationMarker />
 
-          <LayersControl.BaseLayer name="BlackAndWhite">
-            <TileLayer
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
-            />
-          </LayersControl.BaseLayer>
+              {eventsData.map((event) => {
+                return (
+                  <EventMarker key={event.event_id}
+                    event_id={event.event_id}
+                    event_category={event.event_category}
+                    event_title={event.event_title}
+                    event_description={event.event_description}
+                    event_location={event.event_location}
+                    event_country={event.event_country}
+                    event_city={event.event_city}
+                    event_postalcode={event.event_postalcode}
+                    event_address={event.event_address}
+                    event_gps_latitude={event.event_gps_latitude}
+                    event_gps_longitude={event.event_gps_longitude}
+                    event_host_phone={event.event_host_phone}
+                    event_host_email={event.event_host_email}
+                    event_price={event.event_price}
+                    event_start_date={event.event_start_date}
+                    event_end_date={event.event_end_date}
+                    open_spots={event.open_spots}
+                  />
+                )
+              }
+              )
+              }
 
-          <LocationMarker />
+            </LayersControl>
+          </MapContainer>
 
-          {eventsData.map((event) => {
-            return (
-              <EventMarker
-                event_id={event.event_id}
-                event_category={event.event_category}
-                event_title={event.event_title}
-                event_description={event.event_description}
-                event_location={event.event_location}
-                event_country={event.event_country}
-                event_city={event.event_city}
-                event_postalcode={event.event_postalcode}
-                event_address={event.event_address}
-                event_gps_latitude={event.event_gps_latitude}
-                event_gps_longitude={event.event_gps_longitude}
-                event_host_phone={event.event_host_phone}
-                event_host_email={event.event_host_email}
-                event_price={event.event_price}
-                event_start_date={event.event_start_date}
-                event_end_date={event.event_end_date}
-                open_spots={event.open_spots}
-              />
-            )
-          }
-          )
-          }
-
-        </LayersControl>
-      </MapContainer>
-
+        </div>
+      </div>
     </div>
   )
 }
